e3861c4b5586976919ce0f30bec35cb1be299f1d
Farhanachowdhury99 <farhana.chowdhury99@gmail.com>
2021-03-06 00:54:23 -0500

    Homework submission

--- src/amazon/SubHeaderTest.java
+++ src/amazon/SubHeaderTest.java
@@ -1,5 +1,6 @@
 package amazon;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,8 +16,8 @@ public class SubHeaderTest extends SubHeader {
     @Test
     public void testBestSellerLink(){
         checkBestSellerLink();
-        String expectedText="Amazon.com Best Sellers: The most popular items on Amazon";
-        String actualText = driver.getTitle();
+        String expectedText="Baby";
+        String actualText = driver.findElement(By.xpath("//*[@id=\"zg_browseRoot\"]/ul/li[8]/a")).getText();
         Assert.assertEquals(actualText, expectedText, "Best sellers link on the sub-header is not working as expected");
     }
 
@@ -28,12 +29,20 @@ public class SubHeaderTest extends SubHeader {
         Assert.assertEquals(actualText, expectedText, "Best sellers appliances Link is not working");
     }
 
+
+//    @Test
+//    public void testBestSellersBooksLink(){
+//        checkBestSellerBooksLink();
+//        String expectedText="Amazon Best Sellers: Best Children's Books";
+//        String actualText= driver.getTitle();
+//        Assert.assertEquals(actualText,expectedText,"Best sellers Books Link is not working");
+//    }
     @Test
     public void testBestSellersBooksLink(){
         checkBestSellerBooksLink();
-        String expectedText="Amazon Best Sellers: Best Children's Books";
-        String actualText= driver.getTitle();
-        Assert.assertEquals(actualText,expectedText,"Best selles Books Link is not working");
+        String expectedText="Amazon Book Box";
+        String actualText= driver.findElement(By.xpath("//*[@id=\"a-page\"]/div[2]/div[2]/div[2]/div[1]/div[1]/ul[2]/li[1]/a")).getText();
+        Assert.assertEquals(actualText,expectedText,"Best sellers Books Link is not working");
     }
 
     @Test
@@ -47,16 +56,16 @@ public class SubHeaderTest extends SubHeader {
     @Test
     public void testBestSellerAppsAndGames(){
         checkAppsAndGamesLink();
-        String expectedText="Amazon Best Sellers: Best Books & Comics";
-        String actualText=driver.getTitle();
+        String expectedText="Readers & Players";
+        String actualText=driver.findElement(By.cssSelector("#zg_browseRoot > ul > ul > ul > li:nth-child(2) > a")).getText();
         Assert.assertEquals(actualText,expectedText,"Best sellers apps and games Link is not working");
     }
 
     @Test
     public void testTodaysDealLink(){
         checkTodaysDealLink();
-        String expectedText="Gold Box Deals | Today's Deals - Amazon.com";
-        String actualText=driver.getTitle();
+        String expectedText="Home";
+        String actualText=driver.findElement(By.xpath("//*[@id=\"widgetFilters\"]/div/div[3]/div[8]/span/a/div/div[2]/span")).getText();
         Assert.assertEquals(actualText,expectedText,"Today's deals link  is not working");
     }
 
@@ -71,7 +80,7 @@ public class SubHeaderTest extends SubHeader {
     @Test
     public void testAutomotiveLink(){
         checkAutomotiveLink();
-        String expectedText="Amazon.com: Car Care: Automotive: Tools & Equipment, Finishing, Interior Care, Exterior Care & More";
+        String expectedText="Amazon Best Sellers: Best Car Care";
         String actualText=driver.getTitle();
         Assert.assertEquals(actualText,expectedText,"Automotive link is not  working");
     }
@@ -79,8 +88,8 @@ public class SubHeaderTest extends SubHeader {
     @Test
     public void testCouponsLink(){
         checkCouponsLink();
-        String expectedText="Amazon Coupons @ Amazon.com";
-        String actualText=driver.getTitle();
+        String expectedText="Most Popular";
+        String actualText=driver.findElement(By.xpath("//*[@id=\"a-page\"]/div[2]/div[2]/div[2]/div/div[1]/ul[2]/li[1]/a")).getText();
         Assert.assertEquals(actualText,expectedText,"Coupons link under Today's Deals is not  working as expected");
     }
 
@@ -111,8 +120,8 @@ public class SubHeaderTest extends SubHeader {
     @Test
     public void testFashionLink(){
         checkFashionLink();
-        String expectedText="Women's Clothing, Shoes, Jewelry, Watches & Handbags | Amazon.com";
-        String actualText=driver.getTitle();
+        String expectedText="Handbags";
+        String actualText=driver.findElement(By.xpath("//*[@id=\"sobe_d_b_4_3\"]/a")).getText();
        Assert.assertEquals(actualText,expectedText,"Womens link is not working");
     }
 
@@ -127,18 +136,25 @@ public class SubHeaderTest extends SubHeader {
     @Test
     public void testTextbooksLink(){
         checkTextbookLink();
-        String expectedText="Amazon.com: Save up to 90% on rental, new, used, and digital textbooks";
-        String actualText=driver.getTitle();
+        String expectedText="eTextbooks";
+        String actualText=driver.findElement(By.xpath("//*[@id=\"a-page\"]/div[2]/div[2]/div[2]/div[1]/div[1]/ul[2]/li[1]/a")).getText();
         Assert.assertEquals(actualText,expectedText,"Textbook link is not working");
     }
 
-    @Test
-    public void testKindleBooksLink(){
-        checkKindleBooks();
-        String expectedText="Amazon.com: Kindle eBooks: Kindle Store: Nonfiction, Literature & Fiction, Foreign Languages, Business & Money & More";
-        String actualText=driver.getTitle();
-        Assert.assertEquals(actualText,expectedText,"Kindle eBook link is not working");
-    }
+//    @Test
+//    public void testKindleBooksLink(){
+//        checkKindleBooks();
+//        String expectedText="Amazon.com: Kindle eBooks: Kindle Store: Nonfiction, Literature & Fiction, Foreign Languages, Business & Money & More";
+//        String actualText=driver.getTitle();
+//        Assert.assertEquals(actualText,expectedText,"Kindle eBook link is not working");
+//    }
+@Test
+public void testKindleBooksLink(){
+    checkKindleBooks();
+    String expectedText="Kindle Book Deals";
+    String actualText=driver.findElement(By.xpath("//*[@id=\"a-page\"]/div[2]/div[2]/div[2]/div[1]/div[1]/ul[1]/li[1]/a")).getText();
+    Assert.assertEquals(actualText,expectedText,"Kindle eBook link is not working");
+}
 
     @Test
     public void testOutletLink(){
@@ -148,29 +164,50 @@ public class SubHeaderTest extends SubHeader {
         Assert.assertEquals(actualText,expectedText,"Amazon outlet link is not working");
     }
 
+//    @Test
+//    public void testCheckBeautyAndPersonalCare(){
+//        checkBeautyAndPersonalCare();
+//        String expectedText="Amazon Best Sellers: Best Perfumes & Fragrances";
+//        String actualText=driver.getTitle();
+//        Assert.assertEquals(actualText,expectedText,"Amazon beauty and personal care link is not working");
+//    }
     @Test
     public void testCheckBeautyAndPersonalCare(){
         checkBeautyAndPersonalCare();
-        String expectedText="Amazon Best Sellers: Best Perfumes & Fragrances";
-        String actualText=driver.getTitle();
+        String expectedText="Women's";
+        String actualText=driver.findElement(By.xpath("//*[@id=\"zg_browseRoot\"]/ul/ul/ul/li[5]/a")).getText();
         Assert.assertEquals(actualText,expectedText,"Amazon beauty and personal care link is not working");
     }
 
-    @Test
-    public void testCheckMakeupLink(){
-        checkMakeupLink();
-        String expectedText="Amazon Best Sellers: Best Makeup" ;
-        String actualText=driver.getTitle();
-        Assert.assertEquals(actualText,expectedText,"Makeup link is not working");
-    }
+//    @Test
+//    public void testCheckMakeupLink(){
+//        checkMakeupLink();
+//        String expectedText="Amazon Best Sellers: Best Makeup" ;
+//        String actualText=driver.getTitle();
+//        Assert.assertEquals(actualText,expectedText,"Makeup link is not working");
+//    }
+@Test
+public void testCheckMakeupLink(){
+    checkMakeupLink();
+    String expectedText="Makeup Palettes" ;
+    String actualText=driver.findElement(By.cssSelector("#zg_browseRoot > ul > ul > ul > li:nth-child(5) > a")).getText();
+    Assert.assertEquals(actualText,expectedText,"Makeup link is not working");
+}
 
-    @Test
-    public void testBestSellerDigitalMusic(){
-        checkBestSellerDigitalMusic();
-        String expectedText="Amazon Best Sellers: Best Digital Music" ;
-        String actualText=driver.getTitle();
-        Assert.assertEquals(actualText,expectedText,"Best Seller digital music link is not working");
-    }
+//    @Test
+//    public void testBestSellerDigitalMusic(){
+//        checkBestSellerDigitalMusic();
+//        String expectedText="Amazon Best Sellers: Best Digital Music" ;
+//        String actualText=driver.getTitle();
+//        Assert.assertEquals(actualText,expectedText,"Best Seller digital music link is not working");
+//    }
+@Test
+public void testBestSellerDigitalMusic(){
+    checkBestSellerDigitalMusic();
+    String expectedText="Albums" ;
+    String actualText=driver.findElement(By.xpath("//*[@id=\"zg_browseRoot\"]/ul/ul/li[1]/a")).getText();
+    Assert.assertEquals(actualText,expectedText,"Best Seller digital music link is not working");
+}
 
     @Test
     public void testBestSellerCameraAndPhoto(){
@@ -180,21 +217,35 @@ public class SubHeaderTest extends SubHeader {
         Assert.assertEquals(actualText,expectedText,"Best Seller camera &  link is not working");
     }
 
-    @Test
-    public void testAwardWinnerBooks(){
+//    @Test
+//    public void testAwardWinnerBooks(){
+//        checkAwardWinnerBooks();
+//    String expectedText="Amazon.com: Book Awards: Books" ;
+//    String actualText=driver.getTitle();
+//    Assert.assertEquals(actualText,expectedText,"Award winner books link is not working");
+//}
+@Test
+public void testAwardWinnerBooks(){
     checkAwardWinnerBooks();
-    String expectedText="Amazon.com: Book Awards: Books" ;
-    String actualText=driver.getTitle();
+    String expectedText="Hugo Awards" ;
+    String actualText=driver.findElement(By.xpath("//*[@id=\"s-refinements\"]/div[1]/ul/li[4]/span/a/span")).getText();
     Assert.assertEquals(actualText,expectedText,"Award winner books link is not working");
-    }
+}
 
-    @Test
-    public void testBestBooksOfTheMonth(){
-        checkBestBooksOfTheMonth();
-        String expectedText="Amazon Books Editors @ Amazon.com" ;
-        String actualText=driver.getTitle();
-        Assert.assertEquals(actualText,expectedText,"Best books of the month link is not working");
-    }
+//    @Test
+//    public void testBestBooksOfTheMonth(){
+//        checkBestBooksOfTheMonth();
+//        String expectedText="Amazon Books Editors @ Amazon.com" ;
+//        String actualText=driver.getTitle();
+//        Assert.assertEquals(actualText,expectedText,"Best books of the month link is not working");
+//    }
+@Test
+public void testBestBooksOfTheMonth(){
+    checkBestBooksOfTheMonth();
+    String expectedText="Nonfiction" ;
+    String actualText=driver.findElement(By.xpath("//*[@id=\"a-page\"]/div[2]/div[2]/div[2]/div[1]/div[1]/ul[2]/li[7]/a")).getText();
+    Assert.assertEquals(actualText,expectedText,"Best books of the month link is not working");
+}
 
     @Test
     public void testKindleBookDeals(){
@@ -220,14 +271,14 @@ public class SubHeaderTest extends SubHeader {
         Assert.assertEquals(actualText,expectedText,"Best sellers professional beauty link is not working");
     }
 
-    @Test
-    public void testBestSellersPersonalCare(){
-      checkBestSellersPersonalCare();
-        checkProfessionalBeauty();
-        String expectedText="Amazon.com: Personal Care" ;
-        String actualText=driver.getTitle();
-        Assert.assertEquals(actualText,expectedText,"Best sellers personal care link is not working");
-    }
+@Test
+public void testBestSellersPersonalCare(){
+    checkBestSellersPersonalCare();
+    checkProfessionalBeauty();
+    String expectedText="Bath & Bathing Accessories" ;
+    String actualText=driver.findElement(By.xpath("//*[@id=\"s-refinements\"]/div[1]/ul/li[3]/span/a/span")).getText();
+    Assert.assertEquals(actualText,expectedText,"Best sellers personal care link is not working");
+}
 
     @Test
     public void testBestSellersGiftCardLink(){
